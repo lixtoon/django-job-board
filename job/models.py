035987ed38a315e97013d0aa29e5d6676e0f1018
lixtoon <belgherbimedamine@gmail.com>
2020-06-08 14:36:14 +0100

    updating job_list.html pagination

--- job/models.py
+++ job/models.py
@@ -6,9 +6,10 @@ JOB_TYPE = (
     ('Part Time','Part Time'),
 )
 
-def image_uplaod(instance,filename):
+def image_upload(instance,filename):
     imagename, extension = filename.split(".")
     return "jobs/%s.%s"%(instance.id,extension)
+
 class Job(models.Model): #table
     title = models.CharField(max_length=100) #column
     # location
@@ -19,7 +20,7 @@ class Job(models.Model): #table
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to=image_uplaod)
+    image = models.ImageField(upload_to=image_upload)
 
 
     def __str__(self):
